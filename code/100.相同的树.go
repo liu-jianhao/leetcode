fa8508f1480798194180040ae35fb83aa67d39be
liujianhao <liujianhao@bytedance.com>
2020-08-07 16:26:43 +0800

    8.7
    Change-Id: I72b252cd5d2094266fc22201bfb4095731179287

---
+++ code/100.相同的树.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=golang
+ *
+ * [100] 相同的树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil && q != nil {
+		return false
+	}
+	if p != nil && q == nil {
+		return false
+	}
+	if p.Val == q.Val {
+		return isSameTree(p.Right, q.Right) && isSameTree(p.Left, q.Left)
+	}
+	return false
+}
+// @lc code=end
+
